bc36cbf763e58bf8f1f11243c307cf3d0d6d0e20
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-09-08 09:13:54 -0700

    documented the regUser method on the usrController.js

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,6 +1,12 @@
 angular.module('userController', [])
 
 .controller('regCtrl', function($scope, $http) {
+    
+    /*
+    regUser:    1. TAKES THE DATA FROM THE REGISTER FORM.
+                2. SENDS THE DATA TO THE BACK-END USING AN HTTP POST REQUIEST.
+                3. RECIVES THE RESPOND FROM THE BACK-END AND CALLS HandleMsgs() METHOD.
+    */
     $scope.regUser = function(regData) {
 
         $http.post('/api/users', $scope.regData)
@@ -15,7 +21,7 @@ angular.module('userController', [])
             })
     }
     /*
-    HANDLEMSGS :    1. RECIVES THE DATA THAT COMES FROM THE SERVER.
+    HandleMsgs :    1. RECIVES THE DATA THAT COMES FROM THE SERVER.
                     2. FIGURES OUT IF IT WAS SUCCESS OR AN ERROR MESSAGE.
                     3. STORES THE MESSAGE(S) IN THE APPROPRIATE VARIABLE.
     */
