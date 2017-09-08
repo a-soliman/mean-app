5491ca2e8b7ed8e8d2c4aabfe10281682d186f96
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-09-08 10:29:26 -0700

    finished the regData functions, and controller

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,13 +1,22 @@
 angular.module('userController', [])
 
-.controller('regCtrl', function($scope, $http) {
-    
+.controller('regCtrl', function($scope, $http, $timeout, $location, $window) {
+
     /*
     regUser:    1. TAKES THE DATA FROM THE REGISTER FORM.
-                2. SENDS THE DATA TO THE BACK-END USING AN HTTP POST REQUIEST.
-                3. RECIVES THE RESPOND FROM THE BACK-END AND CALLS HandleMsgs() METHOD.
+                2. RESETS THE ERROR MESSAGES TO NULL AND THE LOADING STATE TO TRUE FOR BETTER LAYOUT PERFORMANCE.
+                3. CALLS beautify() METHOD TO CLEAN UP THE DATA BEFORE RETURNING THEM BACK.
+                4. SENDS THE DATA TO THE BACK-END USING AN HTTP POST REQUIEST.
+                5. RECIVES THE RESPOND FROM THE BACK-END AND SETS THE LOADING STATE TO FALSE.
+                6. CALLS HandleMsgs() METHOD.
+                7. CALLS REDIRECTPATH() METHOD TO REDIRECT THE LOCATION TO HOME.
     */
     $scope.regUser = function(regData) {
+        $scope.loading = true;
+        $scope.errorMsg = null;
+        $scope.errorMsgs = null;
+
+        regData = $scope.beautify(regData);
 
         $http.post('/api/users', $scope.regData)
             .then(function(data) {
@@ -16,27 +25,56 @@ angular.module('userController', [])
                 console.log("Message : " + data.data.message);
 
                 //display messages
+                $scope.loading = false;
+
                 $scope.handleMsgs(data);
+
+                if(data.data.success) {
+                    $scope.redirectPath();
+                }
                 
             })
     }
+
+
+    $scope.beautify = function(regData) {
+        // grap the data and clean them up
+        return regData;
+    }
+
     /*
     HandleMsgs :    1. RECIVES THE DATA THAT COMES FROM THE SERVER.
                     2. FIGURES OUT IF IT WAS SUCCESS OR AN ERROR MESSAGE.
                     3. STORES THE MESSAGE(S) IN THE APPROPRIATE VARIABLE.
+                    4. IF THE MESSAGE-STATE WAS SUCCESS, THEN RELOCATE TO ('/').
     */
     $scope.handleMsgs = function(data) {
         if(data.data.sucess) {
             $scope.successMsg = data.data.message;
+
         } else {
             if(data.data.messages) {
+
                 $scope.errorMsg = null;
                 $scope.errorMsgs = data.data.messages;
+
             } else {
+
                 $scope.errorMsgs = null;
                 $scope.errorMsg = data.data.message;
             }
         }
+        return;
+    }
+
+    /*
+    redirectPath:   1. SETS A TIMEOUT FUNCTION OF 200 AND REDIRECTS TO THE HOME PAGE.
+    */
+
+    $scope.redirectPath = function() {
+        $timeout(function() {
+            $location.path('/');
+        }, 200)
     }
 
 })
