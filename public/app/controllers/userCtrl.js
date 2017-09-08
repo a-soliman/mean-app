005087912933b372157c6bbd1148619e7980bc39
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-09-08 09:09:02 -0700

    documented handleMsgs function in the userController.js file

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -10,15 +10,27 @@ angular.module('userController', [])
                 console.log("Message : " + data.data.message);
 
                 //display messages
-                if(data.data.sucess) {
-                    $scope.successMsg = data.data.message;
-                } else {
-                    if(data.data.messages) {
-                        $scope.errorMsgs = data.data.messages;
-                    } else {
-                        $scope.errorMsg = data.data.message;
-                    }
-                }
+                $scope.handleMsgs(data);
+                
             })
     }
+    /*
+    HANDLEMSGS :    1. RECIVES THE DATA THAT COMES FROM THE SERVER.
+                    2. FIGURES OUT IF IT WAS SUCCESS OR AN ERROR MESSAGE.
+                    3. STORES THE MESSAGE(S) IN THE APPROPRIATE VARIABLE.
+    */
+    $scope.handleMsgs = function(data) {
+        if(data.data.sucess) {
+            $scope.successMsg = data.data.message;
+        } else {
+            if(data.data.messages) {
+                $scope.errorMsg = null;
+                $scope.errorMsgs = data.data.messages;
+            } else {
+                $scope.errorMsgs = null;
+                $scope.errorMsg = data.data.message;
+            }
+        }
+    }
+
 })
